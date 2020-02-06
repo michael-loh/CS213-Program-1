c996651f9a1e71424ce8b480b0a7298f7aa49af1
michael-loh <54383090+michael-loh@users.noreply.github.com>
2020-02-06 12:31:29 -0500

    Update Team.java

--- src/Team.java
+++ src/Team.java
@@ -4,48 +4,107 @@
  */
 public class Team 
 {
-   private final int NOT_FOUND = -1;
-   private final int GROW_SIZE = 4; //initial and grow size
-   private TeamMember [] team;
-   private int numMembers;
+
+	private final int NOT_FOUND = -1;
+	private final int GROW_SIZE = 4; //initial and grow size
+	private TeamMember [] team;
+	private int numMembers;
+	
+	public Team()
+	{
+		//this is the default constructor
+		team = new TeamMember[GROW_SIZE];
+		numMembers = 0;
+	}
    
-   public Team()
-   {
-      //this is the default constructor
-   }
+	private int find(TeamMember m)
+	{
+		for(int i = 0; i < numMembers; i++) {
+			if(m.equals(team[i])) return i;
+		}
+		return -1;
+	}
    
-   private int find(TeamMember m)
-   {
-       
-   }
+	private void grow()
+	{
+		TeamMember[] newTeam = new TeamMember[ team.length + GROW_SIZE ];
+		for(int i = 0; i < team.length; i++) {
+			newTeam[i] = team[i];
+		}
+		team = newTeam;
+	}
    
-   private void grow()
-   {
-       
-   }
+	public boolean isEmpty()
+	{
+		if(numMembers == 0) {
+			return true;
+		}
+		return false;
+	}
    
-   public boolean isEmpty()
-   {
-       
-   }
+	public void add(TeamMember m)
+	{     
+		if(numMembers >= team.length) {
+			grow();
+		}
+		team[numMembers] = m;
+		numMembers++;
+	}
    
-   public void add(TeamMember m)
-   {     
-            
-   }
+	public boolean remove(TeamMember m)
+	{
+		int index =  find(m);
+		
+		if(index == -1) return false;
+		
+		for(int i = index; i < numMembers-1; i++) {
+			team[i] = team[i+1];
+		}
+		numMembers--;
+		team[numMembers] = null;
+		return true;
+	} 
    
-   public boolean remove(TeamMember m)
-   {
-       
-   } 
+	public boolean contains(TeamMember m)
+	{
+		for(int i = 0; i < numMembers; i++) {
+			if( m.equals(team[i]) ) return true;
+		}
+		return false;
+	} 
    
-   public boolean contains(TeamMember m)
-   {
-      
-   } 
-   
-   public void print()
-   {
-      //set up a for loop and call the toString() method
-   } 
+	public void print()
+	{
+		//set up a for loop and call the toString() method
+		for(int i = 0; i < numMembers; i++) {
+			System.out.println(team[i].toString());
+		}
+	}
+	
+	public static void main(String[]args) {
+		Team team = new Team();
+		
+		TeamMember t1 = new TeamMember("Bob", new Date("1/31/2019"));
+		team.add(t1);
+		team.print();
+		
+		TeamMember t2 = new TeamMember("Karen", new Date("2/14/2019"));
+		team.add(t2);
+		team.print();
+		
+		System.out.println(team.contains(t2));
+		
+		team.remove(t2);
+		team.print();
+		
+		System.out.println(team.contains(t2));
+		
+		System.out.println(team.isEmpty());
+		
+		team.remove(t1);
+		team.print();
+		
+		System.out.println(team.isEmpty());
+		
+	}
 }
