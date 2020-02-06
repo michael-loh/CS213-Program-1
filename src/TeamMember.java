2726194a2e619dffdac31cf6d1b52998ebacef3f
michael-loh <54383090+michael-loh@users.noreply.github.com>
2020-02-06 12:30:47 -0500

    Update TeamMember.java

--- src/TeamMember.java
+++ src/TeamMember.java
@@ -2,34 +2,52 @@
  
  @author  
  */
+
 public class TeamMember 
 {
-   private String name;
-   private Date   startDate;
-   
-   public TeamMember(String nm, Date date)
-   {
-       
-   }
-   
-   public Date getStartDate()
-   {
-      
-   }
-   
-   public boolean equals(Object obj)
-   {
-      //name and startDate must be the same
-   }  
-   
-   public String toString()
-   {
-       //name + " " + startDate;
-   }
 
-   public static void main(String [] args)
-   {
+	private String name;
+	private Date startDate;
+   
+	public TeamMember(String nm, Date date)
+	{
+       this.name = nm;
+       this.startDate = date;
+	}
+   
+	public Date getStartDate()
+	{
+		return startDate;
+	}
+   
+	@Override
+	public boolean equals(Object obj)
+	{		
+		//name and startDate must be the same
+		if( obj == null ) {
+			return false;
+		}
+		if( this.getClass() != obj.getClass() ) {
+			return false;
+		}
+		
+		TeamMember temp = (TeamMember) obj;
+		if( temp.name.equals( this.name ) && temp.getStartDate().equals(this.getStartDate() ) ) {
+			return true;
+		}
+		return false;
+	}  
+   
+	public String toString()
+	{
+		//name + " " + startDate;
+		return name + " " + this.getStartDate().toString();
+	}
+
+   
+	public static void main(String [] args)
+	{
       //testbed main; you must include test cases that exercise 
       //the constructor and all methods in this class.
-   }
+	}
 }
